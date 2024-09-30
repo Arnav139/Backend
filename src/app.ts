94f7853f3cec057a97a8cdff24283e910ad3a5b5
kamran-wstf <ahmad.kamran@thewasserstoff.com>
2024-09-30 18:20:53 +0530

    app.ts updated

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,17 @@
 // user route 
-import userRoutes from './routes/user.router.ts';
-// route declaration 
-// app.use('/api/users', userRoutes);
-// localhost:3000/api/v1/users/
+import userRoutes from './routes/user.router';
+import express from 'express';
+import Documentrouter from './routes/document.router';
+import userRouter from './routes/user.router';
+import envConf from './config/envConf';
 
+const app = express();
+const port = parseInt(envConf.port, 10) || 3000;
 
+app.use('/api/users',Documentrouter);
 
+// app.use('/api/users',userRouter);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
