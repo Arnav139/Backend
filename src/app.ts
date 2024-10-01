74f150dd41632c6c93c9870e2e03aadcf57a39f8
SB-wstf <subha.biswal@thewasserstoff.com>
2024-10-01 13:21:20 +0530

    updated document route

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,18 @@
-// user route 
-import express from 'express';
-import Documentrouter from './routes/document.router';
-import userRouter from './routes/user.router';
-import envConf from './config/envConf';
-import connectDB from './config/db'
+// user route
+import express from "express";
+import Documentrouter from "./routes/document.router";
+import userRouter from "./routes/user.router";
+import envConf from "./config/envConf";
+import connectDB from "./config/db";
 
 const app = express();
 const port = parseInt(envConf.port, 10) || 8000;
-app.use((express.json()))
-app.use('/api/users',Documentrouter);
+app.use(express.json());
+app.use("/api/documents", Documentrouter);
 
-app.use('/api/users',userRouter);
+app.use("/api/users", userRouter);
 
-app.listen(port, async() => {
-  await connectDB();
-  console.log(`Server is running on port ${port}`);
+app.listen(port, async () => {
+    await connectDB();
+    console.log(`Server is running on port ${port}`);
 });
