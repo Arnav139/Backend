a1a7e37b8fedd825c48ce44a292ca6b7b3a1904e
SB-wstf <subha.biswal@thewasserstoff.com>
2024-10-07 14:36:27 +0530

    add global api rate limiter

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,22 @@
 import express from "express";
 import cors from "cors";
+import rateLimit from "express-rate-limit";
 import connectDB from "./config/db";
 import routes from "./routes";
 
 const app = express();
 const port = 8000;
+
+// rate limit settings
+const apiLimiter = rateLimit({
+    windowMs: 5 * 60 * 1000,
+    max: 50,
+    message: "Too many requests from this IP, please try again after 5 minutes.",
+    standardHeaders: true,
+    legacyHeaders: false,
+});
+
+app.use(apiLimiter);
 
 app.use(
     cors({
