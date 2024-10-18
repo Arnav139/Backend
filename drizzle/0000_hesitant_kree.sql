4ebed88e01acdf9c0fc9b26f252807d0685bee7c
Aman Upadhyay <amanupadhyay1211@gmail.com>
2024-10-18 12:54:29 +0530

    Add google login test

---
+++ drizzle/0000_hesitant_kree.sql
@@ -0,0 +1,50 @@
+CREATE TABLE IF NOT EXISTS "documents" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"user_id" integer NOT NULL,
+	"content" text NOT NULL,
+	"metadata" jsonb NOT NULL,
+	"keyword" jsonb,
+	"is_deleted" boolean DEFAULT false,
+	"is_favorite" boolean DEFAULT false,
+	"created_at" timestamp DEFAULT NOW(),
+	"updated_at" timestamp DEFAULT NOW()
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "payment" (
+	"id" serial NOT NULL,
+	"order_id" varchar,
+	"status" varchar DEFAULT 'pending',
+	"user_id" integer,
+	"ref_id" integer,
+	"method" varchar,
+	"credits" integer,
+	"amount" integer NOT NULL,
+	"created_at" timestamp DEFAULT now(),
+	CONSTRAINT "payment_id_pk" PRIMARY KEY("id")
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "userss" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"first_name" varchar(255) NOT NULL,
+	"last_name" varchar(255) NOT NULL,
+	"email" varchar(255) NOT NULL,
+	"phone_number" varchar(10) NOT NULL,
+	"password" varchar(255) NOT NULL,
+	"credits" varchar DEFAULT '10',
+	"refresh_token" text,
+	"created_at" timestamp DEFAULT NOW(),
+	"updated_at" timestamp DEFAULT NOW(),
+	CONSTRAINT "userss_email_unique" UNIQUE("email")
+);
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "documents" ADD CONSTRAINT "documents_user_id_userss_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."userss"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "payment" ADD CONSTRAINT "payment_user_id_userss_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."userss"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
--- test/index.test.ts
+++ test/index.test.ts
@@ -10,10 +10,10 @@
 //     let fetchMock: FetchMock;
 
 //     beforeEach(() => {
-//         fetchMock = jest.fn() as FetchMock;  
-//         global.fetch = fetchMock;  
+//         fetchMock = jest.fn() as FetchMock;
+//         global.fetch = fetchMock;
 //     });
-     
+
 //     test("Test1: should create a user in DB via /register", async () => {
 //         fetchMock.mockResolvedValueOnce({
 //             status: 201,
@@ -90,8 +90,6 @@
 //     })
 // });
 
-
-
 import request from "supertest";
 import { app, server } from "../src/app"; // Ensure your Express app is exported from app.ts
 import { disconnectDB } from "../src/config/db";
@@ -113,11 +111,12 @@ beforeEach(async () => {
 });
 
 afterAll(async () => {
-  // Close the server after tests
+ // Close the server after tests
   await new Promise<void>((resolve) => {
     server.close(() => {
       resolve();
     });
+    console.log("server closed")
   });
 
   // Disconnect from PostgreSQL
@@ -127,13 +126,13 @@ afterAll(async () => {
 describe("User Registration Tests", () => {
   const testUserEmail = "test_john.doe@example.com"; // Use a unique email for tests
 
-  test("1. Should register a new user successfully", async () => {
+  test.skip("1. Should register a new user successfully", async () => {
     try {
       const response = await request(app).post("/user/register").send({
         firstName: "John",
         lastName: "Doe",
         email: testUserEmail,
-        phoneNumber: 1234567890,  // Ensure the phone number is a number
+        phoneNumber: 1234567890, // Ensure the phone number is a number
         password: "password123",
       });
 
@@ -145,7 +144,8 @@ describe("User Registration Tests", () => {
       expect(response.status).toBe(200);
       expect(response.body.status).toBe(true);
       expect(response.body.message).toBe("User registered successfully");
-      expect(response.body.data).toBe(String);
+      expect(typeof response.body.data).toBe("string");
+      expect(response.body.data).not.toBe(""); // Checks that it is not an empty string
     } catch (error) {
       console.error("Error during user registration:", error);
       throw error; // Rethrow to fail the test
@@ -158,7 +158,7 @@ describe("User Registration Tests", () => {
         firstName: "",
         lastName: "Doe",
         email: testUserEmail,
-        phoneNumber: "1234567890",  // Ensure the phone number is a number
+        phoneNumber: "1234567890", // Ensure the phone number is a number
         password: "password123",
       });
 
@@ -172,6 +172,7 @@ describe("User Registration Tests", () => {
           " Should not register a new user if first name empty",
         errorMessage: "First name is required",
       });
+      expect(response.status).toBe(400);
       expect(response.body.errors["body.firstName"]).toBe(
         "First name is required"
       );
@@ -186,12 +187,15 @@ describe("User Registration Tests", () => {
         firstName: "John",
         lastName: "",
         email: testUserEmail,
-        phoneNumber: "1234567890",  // Ensure the phone number is a number
+        phoneNumber: "1234567890", // Ensure the phone number is a number
         password: "password123",
       });
 
       // Log the request and response
-      console.info(
+      console.info(    // console.log({
+      //   "Test Description": "Login Test",
+      //   errorMessage: "Password should be at least 6 characters",
+      // });
         `Request to register user: ${JSON.stringify(response.body)}`
       );
 
@@ -200,6 +204,7 @@ describe("User Registration Tests", () => {
           " Should not register a new user if Last name empty",
         errorMessage: "Last name is required",
       });
+      expect(response.status).toBe(400);
       expect(response.body.errors["body.lastName"]).toBe(
         "Last name is required"
       );
@@ -214,7 +219,7 @@ describe("User Registration Tests", () => {
         firstName: "John",
         lastName: "Doe",
         email: "iamemail",
-        phoneNumber: "1234567890",  // Ensure the phone number is a number
+        phoneNumber: "1234567890", // Ensure the phone number is a number
         password: "password123",
       });
 
@@ -228,6 +233,7 @@ describe("User Registration Tests", () => {
           " Should not register a new user if Email field is empty",
         errorMessage: "email is required",
       });
+      expect(response.status).toBe(400);
       expect(response.body.errors["body.email"]).toBe("Invalid email");
     } catch (error) {
       console.error("Error during user registration:", error);
@@ -253,6 +259,7 @@ describe("User Registration Tests", () => {
         "Test Description": "Phone number Test",
         errorMessage: "Phone number must be exactly 10 digits long",
       });
+      expect(response.status).toBe(400);
       expect(response.body.errors["body.phoneNumber"]).toBe(
         "Phone number must be exactly 10 digits long"
       );
@@ -280,6 +287,7 @@ describe("User Registration Tests", () => {
         "Test Description": "Password Test",
         errorMessage: "Password should be at least 6 characters",
       });
+      expect(response.status).toBe(400);
       expect(response.body.errors["body.password"]).toBe(
         "Password should be at least 6 characters"
       );
@@ -290,38 +298,40 @@ describe("User Registration Tests", () => {
   });
   test.skip("7. Do not resister already existing user", async () => {
     try {
-        const response = await request(app).post("/user/register").send({
-          firstName: "John",
-          lastName: "Doe",
-          email: testUserEmail,
-          phoneNumber: 1234567890,
-          password: "iampassword",
-        });
+      const response = await request(app).post("/user/register").send({
+        firstName: "John",
+        lastName: "Doe",
+        email: testUserEmail,
+        phoneNumber: 1234567890,
+        password: "iampassword",
+      });
 
-        // Log the request and response
-        console.info(
-          `Request to register user: ${JSON.stringify(response.body)}`
-        );
+      // Log the request and response
+      console.info(
+        `Request to register user: ${JSON.stringify(response.body)}`
+      );
 
-        const response2 = await request(app).post("/user/register").send({
-          firstName: "John",
-          lastName: "Doe",
-          email: testUserEmail,
-          phoneNumber: 1234567890,
-          password: "iampassword",
-        });
+      const response2 = await request(app).post("/user/register").send({
+        firstName: "John",
+        lastName: "Doe",
+        email: testUserEmail,
+        phoneNumber: 1234567890,
+        password: "iampassword",
+      });
 
-        // Log the request and response
-        console.info(
-          `Request to register user: ${JSON.stringify(response2.body)}`
-        );
-        console.log({
-          "Test Description": "Already existing user test",
-          errorMessage: "Error: User already exists with this email",
-        });
-        expect(response2.status).toBe(500);
-        expect(response2.body.status).toBe(false);
-        expect(response2.body.message).toBe("Error: User already exists with this email");
+      // Log the request and response
+      console.info(
+        `Request to register user: ${JSON.stringify(response2.body)}`
+      );
+      console.log({
+        "Test Description": "Already existing user test",
+        errorMessage: "Error: User already exists with this email",
+      });
+      expect(response2.status).toBe(500);
+      expect(response2.body.status).toBe(false);
+      expect(response2.body.message).toBe(
+        "Error: User already exists with this email"
+      );
     } catch (error) {
       console.error("Error during user registration:", error);
       throw error; // Rethrow to fail the test
@@ -329,7 +339,7 @@ describe("User Registration Tests", () => {
   });
 });
 
-describe("User login tests",()=>{
+describe("User login tests", () => {
   const testUserEmail = "test_john.doe@example.com"; // Use a unique email for tests
   test.skip("8. Successfully Login User", async () => {
     try {
@@ -337,30 +347,138 @@ describe("User login tests",()=>{
         firstName: "John",
         lastName: "Doe",
         email: testUserEmail,
-        phoneNumber: 1234567890,  // Ensure the phone number is a number
+        phoneNumber: 1234567890, // Ensure the phone number is a number
         password: "password123",
       });
+      console.info(
+        `Request to register user: ${JSON.stringify(response.body)}`
+      );
+
       const response2 = await request(app).post("/user/login").send({
         email: testUserEmail,
-        password: "1234567890",
+        password: "password123",
       });
 
       // Log the request and response
-      console.info(
-        `Request to register user: ${JSON.stringify(response2.body)}`
-      );
+      console.info(`Request to login user: ${JSON.stringify(response2.body)}`);
+      
+      expect(response2.status).toBe(200);
+      expect(response2.body.status).toBe(true);
+      expect(response2.body.message).toBe("user Logged In");
+      expect(typeof response2.body.data.token).toBe("string");
+      expect(response2.body.data.token).not.toBe("");
 
-      console.log({
-        "Test Description": "Password Test",
-        errorMessage: "Password should be at least 6 characters",
-      });
-      expect(response.body.errors["body.password"]).toBe(
-        "Password should be at least 6 characters"
-      );
     } catch (error) {
-      console.error("Error during user registration:", error);
+      console.error("Error during user login:", error);
       throw error; // Rethrow to fail the test
     }
+  });
+
+  test.skip("9. Should return error for non-existing user", async () => {
+    try {
+      const response = await request(app).post("/user/login").send({
+        email: "nonexistent@example.com",
+        password: "password123",
+      });
+
+      console.info(
+        `Response for login attempt with non-existing user: ${JSON.stringify(
+          response.body
+        )} :  ${JSON.stringify(
+          response.status
+        )} `
+      );
+
+      expect(response.status).toBe(500);
+      expect(response.body.status).toBe(false);
+      expect(response.body.message).toBe("Server error");
+    } catch (error) {
+      console.error("Error during user login:", error);
+      throw error; // Rethrow to fail the test
+    }
+  });
+  // test("10. Should return error for wrong password", async () => {
+  //   try {
+
+  //     const response = await request(app).post("/user/register").send({
+  //       firstName: "John",
+  //       lastName: "Doe",
+  //       email: testUserEmail,
+  //       phoneNumber: 1234567890, // Ensure the phone number is a number
+  //       password: "password123",
+  //     });
+
+  //     const response2 = await request(app).post("/user/login").send({
+  //       email: testUserEmail,
+  //       password: "wrognPassword",
+  //     });
+
+  //     console.info(
+  //       `Response for login attempt with non-existing user: ${JSON.stringify(
+  //         response2.body
+  //       )} :  ${JSON.stringify(
+  //         response2.status
+  //       )} `
+  //     );
+
+  //     expect(response2.status).toBe(500);
+  //     expect(response2.body.status).toBe(false);
+  //     expect(response2.body.message).toBe("Server error");
+  //   } catch (error) {
+  //     console.error("Error during user login:", error);
+  //     throw error; // Rethrow to fail the test
+  //   }
+  // });
+
+  //As login functionality has been handled from frontend only there is no need to write login/logout tests
+
+  describe("User Google Login Test",()=>{
+    test("10. Should login with valid token", async () => {
+      try {
+        const token = process.env.TOKEN
+        console.log(`token:- ${token}`)
+        const response = await request(app).get(`/user/google-login?token=${token}`);
+  
+        console.info(
+          `Response for google login attempt with token: ${JSON.stringify(
+            response.body
+          )} :  ${JSON.stringify(
+            response.status
+          )} `
+        );
+  
+        expect(response.status).toBe(200);
+        expect(response.body.status).toBe(true);
+        expect(response.body.message).toBe("LogedIn with Google");
+        expect(typeof response.body.genToken).toBe("string");
+        expect(response.body.genToken).not.toBe("");
+      } catch (error) {
+        console.error("Error during user login:", error);
+        throw error; // Rethrow to fail the test
+      }
+    });
   })
-})
+    test("11. Should not login with invalid token", async () => {
+      try {
+        const token = process.env.TOKEN
+        console.log(`token:- ${token}`)
+        const response = await request(app).get(`/user/google-login?token=wrogntoken`);
+  
+        console.info(
+          `Response for google login attempt with token: ${JSON.stringify(
+            response.body
+          )} :  ${JSON.stringify(
+            response.status
+          )} `
+        );
+  
+        expect(response.status).toBe(404);
+        expect(typeof response.body).toBe("object");
+        expect(response.body).toEqual({});
+      } catch (error) {
+        console.error("Error during user login:", error);
+        throw error; // Rethrow to fail the test
+      }
+    });
+  })
 
